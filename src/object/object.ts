9bbae9b1200b357d00764f049a944019bf7eb6a5
Cosmin Vasilache <contact@cosminvasilache.dev>
2021-04-02 00:17:58 +0300

    Add object utils safeSetValue() and safeGetValue().

--- src/object/object.ts
+++ src/object/object.ts
@@ -1,15 +1,14 @@
 
-export interface ISafeSetValueOptions {
-    createPath?: boolean,
-    overridePath?: boolean,
-    overrideValue?: boolean,
-}
+export type TObjectKey = string | number; // TODO there should be a built in type
 
-const SAFE_SET_VALUE_DEFAULT_OPTIONS: ISafeSetValueOptions = {
-    createPath: true,
-    overridePath: true,
-    overrideValue: true,
+export type TGenericObject = {
+    [key in TObjectKey]: unknown;
 };
+export interface ISafeSetValueOptions {
+    createPath?: boolean, // if it should create the path if it does not already exist
+    overridePath?: boolean, // if the values encountered along the path are not objects, in order to accomodate traversal, replace them with empty objects
+    overrideValue?: boolean, // if a value already exists at the specified path
+}
 
 /**
  * Assigns the value at the given path inside the object.
@@ -18,15 +17,76 @@ const SAFE_SET_VALUE_DEFAULT_OPTIONS: ISafeSetValueOptions = {
  * If the terminal value exists, overrides it.
  */
 export function safeSetValue(
-    object: Object,
-    path: string[],
+    object: TGenericObject,
+    path: TObjectKey[],
     value: unknown,
     options?: ISafeSetValueOptions,
-): void {
-    options = {
-        ...SAFE_SET_VALUE_DEFAULT_OPTIONS,
+): boolean {
+    if (path.length === 0) {
+        return false;
+    }
+
+    let { createPath, overridePath, overrideValue }: ISafeSetValueOptions = {
+        createPath: true,
+        overridePath: true,
+        overrideValue: true,
+
         ...options,
     };
-    // TODO: implement
 
+    let currentPosition = object;
+    let currentKey;
+
+    for (let i = 0; i < path.length - 1; i++) {
+        currentKey = path[i];
+
+        if (currentPosition[currentKey] === undefined) {
+            if (createPath) {
+                const newObject = {};
+                currentPosition[currentKey] = newObject;
+                currentPosition = newObject;
+            } else {
+                return false;
+            }
+        } else if (typeof currentPosition[currentKey] === 'object') {
+            currentPosition = currentPosition[currentKey] as TGenericObject;
+        } else if (overridePath) {
+            createPath = true; // if you already replaced a value along the path with an empty object, create the entire path from here onward
+            const newObject = {};
+            currentPosition[currentKey] = newObject;
+            currentPosition = newObject;
+        } else {
+            return false;
+        }
+    }
+
+    currentKey = path[path.length - 1];
+    console.log(currentKey);
+    if (currentPosition[currentKey] === undefined) {
+        currentPosition[currentKey] = value;
+    } else if (overrideValue === true) {
+        currentPosition[currentKey] = value;
+    } else {
+        return false;
+    }
+
+    return true;
+}
+
+export function safeGetValue(
+    object: TGenericObject,
+    path: TObjectKey[],
+) {
+    let currentPosition = object;
+    for (let i = 0; i < path.length; i++) {
+        const currentKey = path[i];
+
+        if (currentPosition[currentKey] === undefined) {
+            return undefined;
+        }
+
+        // @ts-expect-error
+        currentPosition = currentPosition[currentKey];
+    }
+    return currentPosition;
 }
