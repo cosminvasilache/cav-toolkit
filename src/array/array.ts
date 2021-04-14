60eb5c2ef10483e2bf4f278baa788a0065ff53eb
Cosmin Vasilache <contact@cosminvasilache.dev>
2021-04-14 19:33:13 +0300

    Add swapItems utility to array module.

--- src/array/array.ts
+++ src/array/array.ts
@@ -36,3 +36,9 @@ export function consolidateArr<T>({
 
     return finalArr;
 }
+
+export function swapItemsInArray<T>(arr: T[], index1: number, index2: number) {
+    const temp = arr[index1];
+    arr[index1] = arr[index2];
+    arr[index2] = temp;
+}
